43c9bfe7750c7aaad4272303c74a16cf4d605b3d
Ashwani Jha <ashwanijha04@gmail.com>
2021-12-03 22:11:30 +0530

    Create sideEffects.js

---
+++ sideEffects.js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [resourceType, setResourceType] = useState("");
+  const [data, setData] = useState([]);
+
+  console.log("Without effect!");
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      .then((res) => {
+        setData(res);
+      });
+  }, [resourceType]);
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <button
+        onClick={() => {
+          setResourceType("posts");
+        }}
+      >
+        Posts
+      </button>
+      <button
+        onClick={() => {
+          setResourceType("comments");
+        }}
+      >
+        Comments
+      </button>
+      <button
+        onClick={() => {
+          setResourceType("todos");
+        }}
+      >
+        Todos
+      </button>
+      <button
+        onClick={() => {
+          setResourceType("users");
+        }}
+      >
+        Users
+      </button>
+
+
+      {
+        data.map(item=>{
+          return <pre> {JSON.stringify(item)} </pre>
+        })
+      }
+
+    </div>
+  );
+}
