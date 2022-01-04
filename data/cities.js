6f2e17aca62fc7d565e9cd4c306dd64bda872302
Ashwani Jha <ashwanijha04@gmail.com>
2022-01-04 22:33:11 +0530

    Add files via upload

---
+++ data/cities.js
@@ -0,0 +1,33 @@
+const cities = [
+    {
+        city: "Goa",
+        country: "India"
+    },
+
+    {
+        city:"Amsterdam",
+        country:"Netherlands"
+    },
+
+    {
+        city:"New York",
+        country:"USA"
+    },
+
+    {
+        city:"Darjeeling",
+        country:"India"
+    },
+
+    {
+        city:"Tokyo",
+        country:"Japan"
+    },
+    
+    {
+        city:"Lonavala",
+        country:"India"
+    },
+]
+
+export default cities;
