4d696dda5a5ff072d6139c418665b83be86c8bf7
Ashwani Jha <ashwanijha04@gmail.com>
2022-01-24 22:00:10 +0530

    Added a new comment for test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import BirthdayComponent from './components/BirthdayComponent';
 import Calculator from './components/Calculator'
 import Toggle from './components/Toggle';
 
+// This is just a test comment which serves no purpose other than educating people
+
 function App() {
   return (
     <div className="App">
